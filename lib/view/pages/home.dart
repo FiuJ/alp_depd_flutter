9abff870d615501d910e13b2c5cd044c0b7167f1
Jevon <winartojevon@gmail.com>
2025-11-17 20:58:24 +0700

    Add View - Home

---
+++ lib/view/pages/home.dart
@@ -0,0 +1,15 @@
+part of 'pages.dart';
+
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
